60e2daac4a86070348a07d28ea5b20103f0ca1ec
Mulle21 <mulukenab95@gmail.com>
2023-02-23 23:23:45 -0800

    task 1-100

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Test for storage
+"""
+from datetime import datetime
+import unittest
+from time import sleep
+import json
+from models.engine.file_storage import FileStorage
